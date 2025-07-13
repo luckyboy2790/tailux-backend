2d43fe07a8044a0591d2a7db8f09ce04af118ded
luckyboy2790 <rorifloris78@gmail.com>
2025-07-13 22:08:41 +0100

    update: update sending mcode part

--- src/models/advancedDelete.model.js
+++ src/models/advancedDelete.model.js
@@ -25,8 +25,8 @@ exports.sendVerificationCode = async (req) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.hostinger.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
